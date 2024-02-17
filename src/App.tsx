3057ad6030924de903f2a72eb6d96f6b52ca576d
deddybrama <dedybramayadi0@gmail.com>
2024-02-17 21:05:13 +0700

    add missing chakra-ui packages and cleanup codes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Box, Heading } from '@chakra-ui/react'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Box } from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <Box>
-      <Heading bgColor={'red'}>Hello</Heading>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -31,7 +30,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </Box>
-  )
+  );
 }
 
-export default App
+export default App;
