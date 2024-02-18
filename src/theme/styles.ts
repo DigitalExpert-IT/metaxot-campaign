8608e645ffb4ee7c882ecf8cb6eaacaee592a150
deddybrama <dedybramayadi@gmail.com>
2024-02-19 00:22:33 +0700

    fix absolute import

--- src/theme/styles.ts
+++ src/theme/styles.ts
@@ -3,13 +3,14 @@ import { Styles } from "@chakra-ui/theme-tools";
 export const styles: Styles = {
   global: {
     body: {
-      height: '100vh',
+      height: "100vh",
       fontFamily: "body",
       color: "chakra-body-text",
       bg: "black",
       transitionProperty: "background-color",
       transitionDuration: "normal",
       lineHeight: "base",
+      display: "block",
     },
     "*::placeholder": {
       color: "chakra-placeholder-color",
