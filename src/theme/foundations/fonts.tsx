b48e2e38e28c75597826d0e64a20e14678bc866b
deddybrama <dedybramayadi@gmail.com>
2024-02-19 15:13:04 +0700

    update implementation for custom fonts

--- src/theme/foundations/fonts.tsx
+++ src/theme/foundations/fonts.tsx
@@ -1,9 +1,11 @@
 import { Global } from "@emotion/react";
+import "@fontsource-variable/urbanist";
+import "@fontsource/unlock";
 
 const Fonts = () => (
   <Global
     styles={`
-      font-family: "Urbanist", sans-serif;
+      font-family: "Urbanist Variable", sans-serif;
       font-optical-sizing: auto;
       font-weight: 400;
       font-style: normal;
