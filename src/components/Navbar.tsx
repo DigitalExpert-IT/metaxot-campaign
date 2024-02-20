2058b74feb1ee6acd829f9c4ca3c63bfddc064a9
deddybrama <dedybramayadi@gmail.com>
2024-02-20 23:12:05 +0700

    fix: navbar position and add missing image alt

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,13 @@
 import metaxotLogoUrl from "../assets/images/metaxot-logo-white.png";
-import { Box, Center, Container, Image, Link, Stack, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Center,
+  Container,
+  Image,
+  Link,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 import WalletButton from "./WalletButton";
 import { INavigation } from "@/constant/navigation";
 
@@ -9,11 +17,11 @@ interface INavbar {
 
 const Navbar: React.FC<INavbar> = ({ data }) => {
   return (
-    <Center >
+    <Center>
       <Stack
         as="nav"
         w={"full"}
-        position={"fixed"}
+        position={"absolute"}
         mt={12}
         top={"0"}
         justifyContent="center"
@@ -21,7 +29,7 @@ const Navbar: React.FC<INavbar> = ({ data }) => {
         <Container maxW="container.xl">
           <Box display="flex" alignItems="center">
             <Box display="flex" justifyContent={"end"} flex={1}>
-              <Image src={metaxotLogoUrl} />
+              <Image src={metaxotLogoUrl} alt={"Metaxot Logo"} />
             </Box>
             <Stack
               flex={1}
