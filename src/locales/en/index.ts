de8e6ebff8892587ce3bd5ef3141624d018a9074
deddybrama <dedybramayadi@gmail.com>
2024-02-26 22:43:51 +0700

    feat: wip metaxot core section

--- src/locales/en/index.ts
+++ src/locales/en/index.ts
@@ -2,12 +2,14 @@ import navigation from "./navigation.json";
 import header from "./header.json";
 import roadmap from "./roadmap.json";
 import theGame from "./theGame.json";
+import metaxotCore from "./metaxotCore.json";
 
 const en = {
   ...navigation,
   ...header,
   ...roadmap,
   ...theGame,
+  ...metaxotCore,
 };
 
 export default en;
---
+++ src/routes/home/MetaxotCore.tsx
@@ -0,0 +1,46 @@
+import { Box, Img, Text } from "@chakra-ui/react";
+import rightTitleShapeUrl from "@/assets/images/title-shape-right.png";
+import theGameUrl from "@/assets/images/the-game.png";
+import { useTranslation } from "react-i18next";
+
+const MetaxotCore: React.FC = () => {
+  const { t } = useTranslation();
+  return (
+    <Box display={"flex"} flexDirection={"column"} gap={6}>
+      <Box display={"flex"} flexDirection={"row"} alignItems={"center"} gap={6}>
+        <Text
+          color={"white"}
+          fontFamily={"unlock"}
+          lineHeight={"3"}
+          fontSize={"3xl"}
+          fontWeight={700}
+        >
+          {t("metaxotCore.title")}
+        </Text>
+        <Img
+          src={rightTitleShapeUrl}
+          alt={"A shape at the right side of title"}
+          maxH={"6px"}
+        />
+      </Box>
+      <Box
+        display={"flex"}
+        flexDirection={{ base: "column", md: "row" }}
+        gap={4}
+      >
+        <Box fontSize={"xl"}>
+          <p style={{ fontFamily: `Urbanist` }}>
+            {t("theGame.content.paragraphOne")}
+          </p>
+          <p style={{ fontFamily: `Urbanist` }}>
+            {t("theGame.content.paragraphTwo")}
+          </p>
+        </Box>
+        {/* TO-DO: Change image to video component when the content is ready. */}
+        <Img src={theGameUrl} alt={"The game thumbnail"} />
+      </Box>
+    </Box>
+  );
+};
+
+export default MetaxotCore;
