08aec761a22e6b083f202f0ac585f091afe2f64d
Dedy Bramayadi <43480898+deddybrama11@users.noreply.github.com>
2024-02-27 08:25:30 +0700

    Feat/the game (#4)
    * create navigation bar
    
    - init i18n
    - add multiple fonts
    - add assets
    
    * Add layout
    
    * fix absolute import
    
    * add missing depedency
    
    * update implementation for custom fonts
    
    * fix: missing en key
    
    * fix: connect wallet button style
    
    * feat: add header section in home page
    
    * fix: message in header.json
    
    * style: update Header.tsx style props using provided value in theme
    
    * chore: cleanup codes
    
    * feat: add roadmap section
    
    * feat: add the game section
    
    * fix: add missing alt for images
    
    * refactor: update image alt
    
    ---------
    
    Co-authored-by: deddybrama11 <dedybramayadi0@gmail.com>

--- src/components/Hexagonal.tsx
+++ src/components/Hexagonal.tsx
@@ -1,19 +1,19 @@
 import { Box, BoxProps } from "@chakra-ui/react";
 
-interface IHexagonal extends BoxProps{}
+interface IHexagonal extends BoxProps {}
 
 const Hexagonal: React.FC<IHexagonal> = (props) => {
-  const {...rest} = props;
+  const { ...rest } = props;
   return (
     <Box
-        top={0}
-        bottom={0}
-        left={0}
-        right={0}
-        fontSize={"160px"}
-        color={'transparent'}
-        position={"relative"}
-        {...rest}
+      top={0}
+      bottom={0}
+      left={0}
+      right={0}
+      fontSize={"160px"}
+      color={"transparent"}
+      position={"relative"}
+      {...rest}
     >
       &#x2B22;
     </Box>
--- src/locales/en/index.ts
+++ src/locales/en/index.ts
@@ -1,11 +1,13 @@
 import navigation from "./navigation.json";
 import header from "./header.json";
 import roadmap from "./roadmap.json";
+import theGame from "./theGame.json";
 
 const en = {
   ...navigation,
   ...header,
   ...roadmap,
+  ...theGame,
 };
 
 export default en;
