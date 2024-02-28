44d1bd5fc76e02ec858fc94a9311f30a8910581e
deddybrama <dedybramayadi@gmail.com>
2024-02-28 22:22:01 +0700

    feat: add internalization

--- src/locales/en/index.ts
+++ src/locales/en/index.ts
@@ -3,13 +3,15 @@ import header from "./header.json";
 import roadmap from "./roadmap.json";
 import theGame from "./theGame.json";
 import footer from "./footer.json";
+import riskAndCurrent from "./riskAndCurrent.json";
 
 const en = {
   ...navigation,
   ...header,
   ...roadmap,
   ...theGame,
-  ...footer
+  ...footer,
+  ...riskAndCurrent
 };
 
 export default en;
